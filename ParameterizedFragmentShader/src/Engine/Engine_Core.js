799dc05f0299618bc0f6615412cf53ce2f610f5e
cesar becerra <sezaru@hotmail.es>
2021-02-23 17:50:53 -0500

    se completaron los ultimos dos proyectos del segundo capitulo

---
+++ ParameterizedFragmentShader/src/Engine/Engine_Core.js
@@ -0,0 +1,34 @@
+var gEngine = gEngine || { };
+gEngine.Core = (function () {
+
+    var mGL = null;
+    var getGL = function () { return mGL; };
+    var initializeWebGL = function (htmlCanvasID) {
+        var canvas = document.getElementById(htmlCanvasID);
+        mGL = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+
+        if (mGL === null) {
+            document.write("<br><b>WebGL is not supported!</b>");
+            return;
+        }
+
+        // now initialize the VertexBuffer
+        gEngine.VertexBuffer.initialize();
+    };
+
+    // Clears the draw area and draws one square
+    var clearCanvas = function (color) {
+        mGL.clearColor(color[0], color[1], color[2], color[3]);  // set the color to be cleared
+        mGL.clear(mGL.COLOR_BUFFER_BIT);      // clear to the color previously set
+    };
+
+    // -- end of public methods
+
+    var mPublic = {
+        getGL: getGL,
+        initializeWebGL: initializeWebGL,
+        clearCanvas: clearCanvas
+    };
+
+    return mPublic;
+}());
