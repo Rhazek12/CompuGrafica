f878a8042ff91f2db8135c3afb1655faaef1510f
cesar becerra <sezaru@hotmail.es>
2021-03-09 18:15:16 -0500

    capitulo 3

---
+++ MatrixTransform/public_html/src/Engine/Renderable.js
@@ -0,0 +1,21 @@
+"use strict";  // Operate in Strict mode such that variables must be declared before used!
+
+function Renderable(shader) {
+    this.mShader = shader;       // the shader for shading this object
+    this.mColor = [1, 1, 1, 1];  // Color for fragment shader
+}
+
+//<editor-fold desc="Public Methods">
+//**-----------------------------------------
+// Public methods
+//**-----------------------------------------
+Renderable.prototype.draw = function (modelTransform) {
+    var gl = gEngine.Core.getGL();
+    this.mShader.activateShader(this.mColor);  // always activate the shader first!
+    this.mShader.loadObjectTransform(modelTransform);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
+Renderable.prototype.setColor = function (color) { this.mColor = color; };
+Renderable.prototype.getColor = function () { return this.mColor; };
+//--- end of Public Methods
+//</editor-fold>
